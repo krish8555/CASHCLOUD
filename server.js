c5aa92e3e70d1dc73233c8c26fe2b044a5fdd7f9
krish8555 <kalpeshkrish.22.cse@iite.indusuni.ac.in>
2024-03-02 11:06:59 +0530

    changes for optimization

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
-const mysql = require("mysql");
+const mysql = require("mysql2"); // Import mysql2 instead of mysql
 
 const app = express();
 const PORT = process.env.PORT || 3000;
